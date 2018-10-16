d5b9c48e9405fdefa7e166343f7a6c714bfc848c
DavidClaude <d_a_claude@163.com>
2018-10-16 22:51:18 +0800

    update

--- Hoxis/Hoxis_Agent.cs
+++ Hoxis/Hoxis_Agent.cs
@@ -11,9 +11,18 @@ namespace DacLib.Hoxis
 		/// </summary>
 		public AgentType agentType {get; private set;}
 
+		/// <summary>
+		/// Hoxis编号
+		/// </summary>
+		/// <value>The hoxis identifier.</value>
 		public Hoxis_ID hoxisId { get; private set;}
 
-
+		/// <summary>
+		/// 自动同步
+		/// 周期性矫正关键信息：位置、朝向、状态等
+		/// </summary>
+		/// <value><c>true</c> if auto syn; otherwise, <c>false</c>.</value>
+		public bool autoSyn { get; set;}
 
 		// Use this for initialization
 		void Start ()
@@ -27,10 +36,11 @@ namespace DacLib.Hoxis
 		/// </summary>
 		/// <param name="tp"> Hoxis类型 </param>
 		/// <param name="id"> Hoxis编号 </param>
-		public void CoFunc (AgentType agentTypeArg, Hoxis_ID hoxisIdArg)
+		public void CoFunc (AgentType agentTypeArg, Hoxis_ID hoxisIdArg, bool autoSynArg = true)
 		{
 			agentType = agentTypeArg;
 			hoxisId = hoxisIdArg;
+			autoSyn = autoSynArg;
 		}
 	
 		// Update is called once per frame
@@ -38,5 +48,12 @@ namespace DacLib.Hoxis
 		{
 		
 		}
+	}
+
+	public enum AgentType {
+		None = 0,
+		Host = 1,
+		Proxy = 2,
+		Immortal = 3
 	}
 }
