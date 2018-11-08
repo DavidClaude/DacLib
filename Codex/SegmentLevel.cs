f9af1449405264a466d6bf057e352fade4eeb2bf
DavidClaude <d_a_claude@163.com>
2018-11-08 22:39:36 +0800

    SegmentLevel Updates

--- Codex/SegmentLevel.cs
+++ Codex/SegmentLevel.cs
@@ -1,89 +1,131 @@
 ﻿using System;
+using DacLib.Generic;
 
 namespace DacLib.Codex
 {
-	public class SegmentLevel
-	{
+    public class SegmentLevel
+    {
+        public const int ERROR_LEVLE_OUT_OF_RANGE = 1;
+        public const int ERROR_THRESHOLD_IS_LESS_EQUAL_THAN_ZERO = 2;
+        public const int ERROR_THRESHOLD_IS_LESS_THAN_PRE = 3;
+        public const int ERROR_THRESHOLD_IS_MORE_THAN_NEXT = 4;
+
         /// <summary>
         /// 级别改变事件
         /// </summary>
-        public event DacLib.Generic.IntForVoid_Handler onLevelChange;
+        public event IntForVoid_Handler onLevelChange;
 
-		/// <summary>
-		/// 程度值
-		/// </summary>
-		/// <value>The degree.</value>
-		public float degree { get; private set; }
+        /// <summary>
+        /// 程度值
+        /// </summary>
+        /// <value>The degree.</value>
+        public float degree { get; private set; }
 
-		/// <summary>
-		/// 级别
-		/// </summary>
-		/// <value>The level.</value>
-		public int level {
-			get {
-				for (int i = 0; i < _thresholds.Length; i++) {
-					if (degree <= _thresholds [i])
-						return i;
-				}
-				return _thresholds.Length - 1;
-			}
-		}
+        /// <summary>
+        /// 级别数
+        /// </summary>
+        public int count
+        {
+            get
+            {
+                return _thresholds.Length;
+            }
+        }
 
-		private float[] _thresholds;
-		private int _lastLevel;
+        /// <summary>
+        /// 级别
+        /// </summary>
+        /// <value>The level.</value>
+        public int level
+        {
+            get
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    if (degree <= _thresholds[i])
+                        return i;
+                }
+                return count - 1;
+            }
+        }
 
-		public SegmentLevel (int count)
-		{
-			degree = 0;
-			_thresholds = new float[count];
-			_lastLevel = level;
-		}
+        /// <summary>
+        /// 进度(当前级别中)
+        /// </summary>
+        public float localRate
+        {
+            get
+            {
+                float start = 0;
+                if (level > 0)
+                    start = _thresholds[level - 1];
+                return (degree - start) / (_thresholds[level] - start);
+            }
+        }
 
-		/// <summary>
-		/// 设置阈值
-		/// </summary>
-		/// <param name="level">Level.</param>
-		/// <param name="val">Value.</param>
-		public void SetThreshold (int level, float val)
-		{
-			if (level < 0 || level >= _thresholds.Length)
-				return;
-			_thresholds [level] = val;
-		}
+        private float[] _thresholds;
+        private int _lastLevel;
 
-		/// <summary>
-		/// 加深程度
-		/// </summary>
-		/// <param name="val">Value.</param>
-		public void Extend (float val)
-		{
-			degree += val;
-			float max = _thresholds [_thresholds.Length - 1];
-			if (degree > max) {
-				degree = max;
-			}
-			if (level != _lastLevel) {
-				OnLevelChange (level);
-				_lastLevel = level;
-			}
-		}
+        public SegmentLevel(int countArg)
+        {
+            degree = 0;
+            _thresholds = new float[countArg];
+            _lastLevel = level;
+        }
 
-		/// <summary>
-		/// 还原
-		/// </summary>
-		public void Init ()
-		{
-			degree = 0;
-			if (level != _lastLevel) {
-				OnLevelChange (level);
-				_lastLevel = level;
-			}
-		}
+        /// <summary>
+        /// 设置阈值
+        /// </summary>
+        /// <param name="level">Level.</param>
+        /// <param name="val">Value.</param>
+        public void SetThreshold(int level, float val, out Ret ret)
+        {
+            if (level < 0 || level >= count)
+            {
+                ret = new Ret(ERROR_LEVLE_OUT_OF_RANGE, "Level:" + level + " is out of range");
+                return;
+            }
+            if (val <= 0)
+            {
+                ret = new Ret(ERROR_THRESHOLD_IS_LESS_EQUAL_THAN_ZERO, "Threshold:" + val + " is less or equal than 0");
+                return;
+            }
+            _thresholds[level] = val;
+            ret = Ret.ok;
+        }
 
-		private void OnLevelChange (int level){
-			if (onLevelChange == null)
-				return;
-			onLevelChange (level);
-		}
-	}
+        /// <summary>
+        /// 加深程度
+        /// </summary>
+        /// <param name="val">Value.</param>
+        public void Extend(float val)
+        {
+            if (val == 0)
+                return;
+            degree += val;
+            float max = _thresholds[count-1];
+            if (degree > max)
+            {
+                degree = max;
+            }
+            if (level != _lastLevel)
+            {
+                onLevelChange(level);
+                _lastLevel = level;
+            }
+        }
+
+        /// <summary>
+        /// 还原
+        /// </summary>
+        public void Recover()
+        {
+            degree = 0;
+            if (level != _lastLevel)
+            {
+                onLevelChange(level);
+                _lastLevel = level;
+            }
+        }
+    }
 }
--- Codex/TS_SegmentLevel.cs
+++ Codex/TS_SegmentLevel.cs
@@ -1,33 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DacLib.Generic;
 using DacLib.Codex;
 
-public class TS_SegmentLevel : MonoBehaviour {
-	SegmentLevel sl = new SegmentLevel (5);
-	public float val;
+public class TS_SegmentLevel : MonoBehaviour
+{
+    SegmentLevel sl = new SegmentLevel(5);
+    public float val;
 
-	// Use this for initialization
-	void Start () {
-		sl.SetThreshold (0, 1f);
-		sl.SetThreshold (1, 2f);
-		sl.SetThreshold (2, 3f);
-		sl.SetThreshold (3, 4f);
-		sl.SetThreshold (4, 5f);
-		sl.onLevelChange += (i) => {
-			Debug.Log ("Level change to: " + i);
-		};
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown (KeyCode.A)) {
-			sl.Extend (val);
-			Debug.Log ("Degree: " + sl.degree);
-		}
-		if (Input.GetKeyDown (KeyCode.I)) {
-			sl.Init ();
-			Debug.Log ("Degree: " + sl.degree);
-		}
-	}
+    // Use this for initialization
+    void Start()
+    {
+        Ret ret;
+        sl.SetThreshold(0, 10, out ret);
+        if (ret.code != 0)
+            Debug.LogError(ret.desc);
+        sl.SetThreshold(1, 20, out ret);
+        if (ret.code != 0)
+            Debug.LogError(ret.desc);
+        sl.SetThreshold(2, 25, out ret);
+        if (ret.code != 0)
+            Debug.LogError(ret.desc);
+        sl.SetThreshold(3, 35, out ret);
+        if (ret.code != 0)
+            Debug.LogError(ret.desc);
+        sl.SetThreshold(4, 50, out ret);
+        if (ret.code != 0)
+            Debug.LogError(ret.desc);
+
+        Debug.Log(sl.level);
+        Debug.Log(sl.degree);
+
+        sl.onLevelChange += (i) =>
+        {
+            Debug.Log("Level change to: " + i);
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            sl.Extend(val);
+            Debug.Log("Degree: " + sl.degree);
+            Debug.Log("Local rate: " + sl.localRate);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            sl.Recover();
+            Debug.Log("Degree: " + sl.degree);
+            Debug.Log("Local rate: " + sl.localRate);
+        }
+    }
 }
