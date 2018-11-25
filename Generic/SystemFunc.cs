bb93eefbc0df03e3bb19e875c59d1cf3e1a32cd7
DavidClaude <d_a_claude@163.com>
2018-11-25 21:56:51 +0800

    add SystemFunc

---
+++ Generic/SystemFunc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DacLib.Generic
+{
+    public static class SystemFunc
+    {
+        public static string GetLocalIP(out Ret ret)
+        {
+            try
+            {
+                string name = Dns.GetHostName();
+                IPHostEntry entry = Dns.GetHostByName(name);
+                foreach (IPAddress ipa in entry.AddressList)
+                {
+                    if (ipa.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        ret = Ret.ok;
+                        return ipa.ToString();
+                    }
+                }
+                ret = new Ret(LogLevel.Warning, 1, "No IPv4 by host name: " + name);
+                return "";
+            }
+            catch (Exception e){
+                ret = new Ret(LogLevel.Error, 2, e.Message);
+                return "";
+            }
+            
+        }
+    }
+}
--- Hoxis/Server/HoxisServer.cs
+++ Hoxis/Server/HoxisServer.cs
@@ -4,30 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DacLib.Generic;
+using DacLib.Codex;
 
 namespace DacLib.Hoxis.Server
 {
     public static class HoxisServer
     {
-        //public static void InitConfig(out Ret ret, string configPath = "") {
-        //    // Read config file
-        //    string path;
-        //    if (configPath != "") { path = configPath; }
-        //    else { path = HoxisConfigs.basicPath + "Configs/hoxis_server.toml"; }
-        //    config = new TomlConfiguration(path, out ret);
-        //    if (ret.code != 0) { OnInitError(ret); return; }
-        //    // Assign ip, port and init the sokcet
-        //    serverIP = config.GetString("socket", "server_ip", out ret);
-        //    if (ret.code != 0) { OnInitError(ret); return; }
-        //    port = config.GetInt("socket", "port", out ret);
-        //    if (ret.code != 0) { OnInitError(ret); return; }
-        //    _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, System.Net.Sockets.ProtocolType.Tcp);
-        //    // Init the extractor
-        //    int size = config.GetInt("socket", "read_buffer_size", out ret);
-        //    if (ret.code != 0) { OnInitError(ret); return; }
-        //    _extractor = new HoxisBytesExtractor(size);
-        //    _extractor.onBytesExtracted += ExtractCb;
-        //    ret = Ret.ok;
-        //}
+        public static TomlConfiguration config { get; private set; }
+
+        public static string ip { get; private set; }
+
+        public static int port { get; private set; }
+
+        public static void InitConfig(out Ret ret, string configPath = "")
+        {
+            // Read config file
+            string path;
+            if (configPath != "") { path = configPath; }
+            else { path = HoxisServerConfig.basicPath + "Configs/hoxis_server.toml"; }
+            config = new TomlConfiguration(path, out ret);
+            if (ret.code != 0) { Console.Write("HoxisServer init error: " + ret.desc); return; }
+            // Assign ip, port and init the sokcet
+            ip = SystemFunc.GetLocalIP(out ret);
+            if (ret.code != 0) { Console.Write("HoxisServer init error: " + ret.desc); return; }
+            port = config.GetInt("socket", "port", out ret);
+            if (ret.code != 0) { Console.Write("HoxisServer init error: " + ret.desc); return; }
+            //_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, System.Net.Sockets.ProtocolType.Tcp);
+            // Init the extractor
+            //int size = config.GetInt("socket", "read_buffer_size", out ret);
+            //if (ret.code != 0) { OnInitError(ret); return; }
+            //_extractor = new HoxisBytesExtractor(size);
+            //_extractor.onBytesExtracted += ExtractCb;
+            ret = Ret.ok;
+        }
+
+
+
     }
 }
