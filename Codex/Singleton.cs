d12e3db396a110bafa8f6ede61eb45bea66c2841
DavidClaude <d_a_claude@163.com>
2019-01-08 21:49:34 +0800

    update

---
+++ Codex/Singleton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DacLib.Codex
+{
+    public class Singleton<T> where T : class, new()
+    {
+        public static T Ins { get { if (_Ins == null) _Ins = new T(); return _Ins; } }
+        private static T _Ins;
+    }
+}
