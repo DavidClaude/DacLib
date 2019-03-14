a955e600a071ca6b00a101b699b1fca5be69987b
DavidClaude <d_a_claude@163.com>
2019-03-14 23:47:51 +0800

    Generic functions

---
+++ Generic/GenericFunc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class GenericFunc{
+
+    /// <summary>
+    /// Get a random item of the given list
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="list"></param>
+    /// <returns></returns>
+    public static T GetRandomOfList<T>(List<T> list)
+    {
+        Random random = new Random();
+        return list[random.Next(0, list.Count - 1)];
+    }
+}
